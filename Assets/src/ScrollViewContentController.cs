60b59df4e5300c1203a41ed5fa56f2a98b283658
Zachary625 <Zachary625@outlook.com>
2016-03-02 13:00:43 +0800

    project content filled

---
+++ Assets/src/ScrollViewContentController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ScrollViewContentController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	void OnTransformChildrenChanged() {
+		RectTransform rectTransform = this.GetComponent<RectTransform> ();
+		Vector2 sizeDelta = rectTransform.sizeDelta;
+		sizeDelta.y = 0;
+		foreach (RectTransform rt in rectTransform) {
+//			sizeDelta.y += rt.sizeDelta.y;
+			sizeDelta.y += rt.GetComponent<LayoutElement>().preferredHeight;
+		}
+		Debug.Log (" recalc: " + sizeDelta.y);
+		rectTransform.sizeDelta = sizeDelta;
+	}
+	public class ListViewItemBehaviour: UIBehaviour {
+		protected override void OnRectTransformDimensionsChange() {
+			RectTransform rt = this.GetComponent<RectTransform> ();
+//			Debug.Log (transform.gameObject.name + " sizeDelta.y: " + rt.sizeDelta.y);
+//			Debug.Log (transform.gameObject.name + " rect.height: " + rt.rect.height);
+		}
+	}
+}
---
+++ Assets/src/UIEventHandler.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+using System.Collections;
+
+public class UIEventHandler : MonoBehaviour {
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void onCreateButtonClicked(GameObject srcObj) {
+		GameObject scrollViewObj = GameObject.Find ("Scroll View");
+		GameObject contentObj = scrollViewObj.transform.Find("Viewport").transform.Find("Content").gameObject;
+
+		GameObject panelObj = new GameObject ();
+		Image image = panelObj.AddComponent<Image> ();
+		image.color = Random.ColorHSV();
+		LayoutElement layoutElement = panelObj.AddComponent<LayoutElement> ();
+		layoutElement.preferredWidth = Random.Range(300, 600);
+		layoutElement.preferredHeight = Random.Range (100, 200);
+
+		Debug.Log (" preferred height: " + layoutElement.preferredHeight);
+
+		panelObj.transform.SetParent (contentObj.transform);
+		return;
+
+		GameObject inputFieldObj = GameObject.Find ("InputField");
+		InputField inputField = inputFieldObj.GetComponent<InputField>();
+		GameObject textObj = new GameObject ();
+		Text text = textObj.AddComponent<Text> ();
+		text.text = inputField.text;
+		text.font = inputFieldObj.transform.Find("Text").GetComponent<Text>().font;
+		textObj.AddComponent<ScrollViewContentController.ListViewItemBehaviour>();
+
+		text.transform.SetParent (contentObj.transform);
+	}
+
+	public void onClearButtonClicked(GameObject srcObj) {
+		Debug.Log (" @ UIEventHandler.onCreateButtonClicked()");
+		GameObject scrollViewObj = GameObject.Find ("Scroll View");
+		GameObject contentObj = scrollViewObj.transform.Find("Viewport").transform.Find("Content").gameObject;
+
+		contentObj.transform.DetachChildren ();
+	}
+}
