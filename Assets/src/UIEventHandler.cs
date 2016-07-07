ce14d146b781b40f358fef9c329816fe7e37b128
Zachary625 <Zachary625@outlook.com>
2016-07-07 19:26:11 +0800

    初步做成聊天的样子
    InputField使用：InputField.text是完整文本，当文本过长时，InputField下的text组件的text不完整

--- Assets/src/UIEventHandler.cs
+++ Assets/src/UIEventHandler.cs
@@ -20,26 +20,27 @@ public class UIEventHandler : MonoBehaviour {
 		GameObject contentObj = scrollViewObj.transform.Find("Viewport").transform.Find("Content").gameObject;
 
 		GameObject panelObj = new GameObject ();
-		Image image = panelObj.AddComponent<Image> ();
-		image.color = Random.ColorHSV();
-		LayoutElement layoutElement = panelObj.AddComponent<LayoutElement> ();
-		layoutElement.preferredWidth = Random.Range(300, 600);
-		layoutElement.preferredHeight = Random.Range (100, 200);
-
-		Debug.Log (" preferred height: " + layoutElement.preferredHeight);
-
-		panelObj.transform.SetParent (contentObj.transform);
-		return;
 
 		GameObject inputFieldObj = GameObject.Find ("InputField");
-		InputField inputField = inputFieldObj.GetComponent<InputField>();
-		GameObject textObj = new GameObject ();
-		Text text = textObj.AddComponent<Text> ();
-		text.text = inputField.text;
-		text.font = inputFieldObj.transform.Find("Text").GetComponent<Text>().font;
-		textObj.AddComponent<ScrollViewContentController.ListViewItemBehaviour>();
+		InputField inputField = inputFieldObj.GetComponent<InputField> ();
+		Text inputText = inputFieldObj.transform.Find ("Text").gameObject.GetComponent<Text>();
 
-		text.transform.SetParent (contentObj.transform);
+		Debug.Log (" @ UIEventHandler.onCreateButtonClicked(): inputField.text.Length: " + inputField.text.Length);
+
+		if (inputField.text == null || inputField.text.Length == 0) {
+			return;
+		}
+		Text text = panelObj.AddComponent<Text> ();
+		text.horizontalOverflow = HorizontalWrapMode.Wrap;
+		text.verticalOverflow = VerticalWrapMode.Truncate;
+		text.color = Color.black;
+		text.text = inputField.text;
+		text.font = inputText.font;
+		text.fontSize = inputText.fontSize;
+
+		panelObj.transform.SetParent (contentObj.transform);
+
+		inputField.text = "";
 	}
 
 	public void onClearButtonClicked(GameObject srcObj) {
