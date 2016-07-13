a9fd65253bb982088a26c4bc1d26d856f9e67c7f
Zachary <zachary625@outlook.com>
2016-07-14 02:05:01 +0800

    PropertyType加入 PropertyPair.Type加入

--- Assets/src/StoryTeller/Utility/PropertyPair.cs
+++ Assets/src/StoryTeller/Utility/PropertyPair.cs
@@ -2,12 +2,62 @@
 
 namespace AssemblyCSharp.StoryTeller.Utility
 {
-	public class PropertyPair<T>
+    public enum PropertyType
+    {
+        String = 0,
+        Int = 1,
+        Double = 2,
+        Bool = 3,
+        TimePointSec = 4,
+        TimeSpanSec = 5,
+    }
+
+    public class PropertyPair
 	{
 		private string key;
-		private T value;
+		private string value;
+        private PropertyType type;
 
-		public PropertyPair ()
+        public string Key
+        {
+            get
+            {
+                return key;
+            }
+
+            set
+            {
+                key = value;
+            }
+        }
+
+        public string Value
+        {
+            get
+            {
+                return value;
+            }
+
+            set
+            {
+                this.value = value;
+            }
+        }
+
+        public PropertyType Type
+        {
+            get
+            {
+                return type;
+            }
+
+            set
+            {
+                type = value;
+            }
+        }
+
+        public PropertyPair ()
 		{
 		}
 	}
